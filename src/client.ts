a9557e4bc99aa6a449042c7443bdb66b9076b37c
xushengfeng <xushengfeng_zg@163.com>
2026-05-04 00:19:38 +0800

    客户端 添加propertiesChanged

--- src/client.ts
+++ src/client.ts
@@ -32,22 +32,43 @@ export class dbusService {
 
 export class dbusObject {
 	op: Omit<dbusClientOp, "interface">;
+	private introspectable: dbusInterfaceRaw;
+	private peer: dbusInterfaceRaw;
+	private properties: dbusInterfaceRaw;
 	constructor(op: typeof dbusObject.prototype.op) {
 		this.op = { ...op };
+		this.introspectable = new dbusInterfaceRaw({
+			...this.op,
+			interface: "org.freedesktop.DBus.Introspectable",
+		});
+		this.peer = new dbusInterfaceRaw({
+			...this.op,
+			interface: "org.freedesktop.DBus.Peer",
+		});
+		this.properties = new dbusInterfaceRaw({
+			...this.op,
+			interface: "org.freedesktop.DBus.Properties",
+		});
 	}
 	async getInterface(name: string) {
-		return new dbusInterface({ ...this.op, interface: name });
+		return new dbusInterface(
+			{ ...this.op, interface: name },
+			{
+				introspectable: this.introspectable,
+				peer: this.peer,
+				properties: this.properties,
+			},
+		);
 	}
 }
 
-export class dbusInterface {
+export class dbusInterfaceRaw {
 	op: dbusClientOp;
 	io: dbusIO;
 	constructor(op: dbusClientOp) {
 		this.op = { ...op };
 		this.io = op.io;
 	}
-
 	call<T extends string = "">(
 		method: string,
 		signature: T = "" as T,
@@ -77,18 +98,45 @@ export class dbusInterface {
 			},
 		};
 	}
+}
+
+export class dbusInterface {
+	op: dbusClientOp;
+	io: dbusIO;
+	private meta: {
+		introspectable: dbusInterfaceRaw;
+		peer: dbusInterfaceRaw;
+		properties: dbusInterfaceRaw;
+	};
+	private m: dbusInterfaceRaw;
+	constructor(
+		op: dbusClientOp,
+		meta: {
+			introspectable: dbusInterfaceRaw;
+			peer: dbusInterfaceRaw;
+			properties: dbusInterfaceRaw;
+		},
+	) {
+		this.op = { ...op };
+		this.io = op.io;
+		this.meta = meta;
+		this.m = new dbusInterfaceRaw(this.op);
+	}
+
+	call<T extends string = "">(
+		method: string,
+		signature: T = "" as T,
+		// @ts-expect-error
+		...args: DBusTypes<T>
+	) {
+		// @ts-expect-error
+		return this.m.call(method, signature, ...args);
+	}
 
 	async get<T extends string>(property: string): Promise<DBusTypes<T>> {
-		const msg = new dbusMessage();
-		msg.setDestination(this.op.destination);
-		msg.setPath(this.op.path);
-		msg.setInterface("org.freedesktop.DBus.Properties");
-		msg.setMember("Get");
-		msg.setSignature("ss");
-		msg.setBody([this.op.interface, property]);
-
-		const response = await this.io.call(msg);
-		return response.getBody() as DBusTypes<T>;
+		return await this.meta.properties
+			.call("Get", "ss", this.op.interface, property)
+			.as<T>();
 	}
 
 	async set<T extends string>(
@@ -96,26 +144,15 @@ export class dbusInterface {
 		value: DBusTypes<T>,
 		signature: T,
 	): Promise<void> {
-		const msg = new dbusMessage();
-		msg.setDestination(this.op.destination);
-		msg.setPath(this.op.path);
-		msg.setInterface("org.freedesktop.DBus.Properties");
-		msg.setMember("Set");
-		msg.setSignature("ssv");
-		msg.setBody([this.op.interface, property, { signature, value }]);
-		await this.io.call(msg);
+		await this.meta.properties
+			.call("Set", "ssv", this.op.interface, property, { signature, value })
+			.await();
 	}
 
 	async getAll(): Promise<Record<string, unknown>> {
-		const msg = new dbusMessage();
-		msg.setDestination(this.op.destination);
-		msg.setPath(this.op.path);
-		msg.setInterface("org.freedesktop.DBus.Properties");
-		msg.setMember("GetAll");
-		msg.setSignature("s");
-		msg.setBody([this.op.interface]);
-		const response = await this.io.call(msg);
-		const b = response.getBody() as DBusTypes<"a{sv}">;
+		const b = await this.meta.properties
+			.call("GetAll", "s", this.op.interface)
+			.as<"a{sv}">();
 		const result: Record<string, unknown> = {};
 		for (const [k, v] of b[0]) {
 			result[k] = v;
@@ -165,5 +202,35 @@ export class dbusInterface {
 			removeMsg.setBody([rule]);
 			await this.io.call(removeMsg);
 		};
+	}
+
+	async propertiesChanged(
+		callback: (
+			changedProperties: Record<string, unknown>,
+			invalidatedProperties: string[],
+		) => void,
+	): Promise<() => void> {
+		const propertiesIface = new dbusInterface(
+			{ ...this.op, interface: this.meta.properties.op.interface },
+			this.meta,
+		);
+		// todo 合并到object
+		return propertiesIface.on<"sa{sv}as">(
+			"PropertiesChanged",
+			(interfaceName, changedProperties, invalidatedProperties) => {
+				if (interfaceName !== this.op.interface) return;
+				callback(
+					Object.fromEntries(changedProperties.map(([k, v]) => [k, v.value])),
+					invalidatedProperties,
+				);
+			},
+		);
+	}
+}
+
+export class dbusMetaInterface {
+	private iface: dbusInterface;
+	constructor(iface: dbusInterface) {
+		this.iface = iface;
 	}
 }
