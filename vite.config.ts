a697030f4e1221660bdae3246049a36168dfafa2
xushengfeng <xushengfeng_zg@163.com>
2026-05-04 18:34:02 +0800

    編译 修复类型打包位置错误

--- vite.config.ts
+++ vite.config.ts
@@ -3,24 +3,23 @@ import path from "path";
 import dts from "vite-plugin-dts";
 
 export default defineConfig({
-    plugins: [dts({ rollupTypes: true })],
-    build: {
-        lib: {
-            entry: path.resolve(__dirname, "src/index.ts"),
-            name: "NodeDBus",
-            fileName: (format) => `index.${format === "es" ? "mjs" : "cjs"}`,
-            formats: ["es", "cjs"],
-        },
-        rollupOptions: {
-            // make sure to externalize deps that shouldn't be bundled
-            // into your library
-            external: ["myde-unix-socket", "child_process", "fs", "path", "events"],
-            output: {
-                // Provide global variables to use in the UMD build
-                // for externalized deps
-                globals: {
-                },
-            },
-        },
-    },
+	plugins: [dts({ tsconfigPath: "./tsconfig.build.json" })],
+	build: {
+		lib: {
+			entry: path.resolve(__dirname, "src/index.ts"),
+			name: "NodeDBus",
+			fileName: (format) => `index.${format === "es" ? "mjs" : "cjs"}`,
+			formats: ["es", "cjs"],
+		},
+		rollupOptions: {
+			// make sure to externalize deps that shouldn't be bundled
+			// into your library
+			external: ["myde-unix-socket", "child_process", "fs", "path", "events"],
+			output: {
+				// Provide global variables to use in the UMD build
+				// for externalized deps
+				globals: {},
+			},
+		},
+	},
 });
